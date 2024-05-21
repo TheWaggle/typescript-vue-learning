0d92a559bc27e4f5e9e4a5db86476cd69e041120
tasuku.kawahara <tasuku.kawahara@givery.co.jp>
2024-05-21 17:18:50 +0900

    Track 受験用 Vue3 テンプレート

---
+++ track/vue3-template/vite.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'public',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith('.css')) {
+            return 'style.css'
+          }
+          return `assets/${assetInfo.name}`
+        }
+      }
+    }
+  }
+})
